4d91c48577e59dcc6aecaeeb26801a14118fe641
CyberConductor <gilad4025@gmail.com>
2025-06-09 18:13:37 +0300

    changed gui instructions

---
+++ WpfApp/RequestsEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public enum Requests
+    {
+        Request_Login = 0,
+        Request_Signup,
+        Request_Signout,
+        Request_GetRooms,
+        Request_GetPlayersInRoom,
+        Request_getStatistics,
+        Request_GetPersonalStats,
+        Request_GetHighScore,
+        Request_JoinRoom,
+        Request_CreateRoom
+    }
+}
