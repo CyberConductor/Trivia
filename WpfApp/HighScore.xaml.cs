3fcd1d6d966b4baafc2fe53583d2fb5487031ed2
CyberConductor <gilad4025@gmail.com>
2025-06-15 01:47:49 +0300

    fixed high score erros

--- WpfApp/HighScore.xaml.cs
+++ WpfApp/HighScore.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Text.Json;
-using TriviaClient.Network;
+using System.Windows;
+using TriviaClient.Network; // Ensure this namespace and class exist
 using WpfApp;
 
 namespace WpfApp1
@@ -30,6 +18,7 @@ namespace WpfApp1
         {
             try
             {
+                // Simulating a request to the server
                 string responseJson = ServerCommunicator.SendRequest((byte)Requests.Request_GetHighScore, "{}");
 
                 using JsonDocument doc = JsonDocument.Parse(responseJson);
