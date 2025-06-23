af834673fe11ef44afdcc3d8340c338ca9938f78
CyberConductor <gilad4025@gmail.com>
2025-06-23 16:17:16 +0300

    added back buttons

--- WpfApp/HighScore.xaml.cs
+++ WpfApp/HighScore.xaml.cs
@@ -63,9 +63,9 @@ namespace WpfApp1
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            Menu menuWindow = new Menu(); 
-            menuWindow.Show();
-            this.Close();
+            Statistics st = new Statistics();
+            st.Show();
+            this.Close(); ;
         }
        
         private void CloseButton_Click(object sender, RoutedEventArgs e)
